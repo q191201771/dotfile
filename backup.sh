f2619f81bf57930502079471272a81005c4abd07
q191201771 <191201771@qq.com>
2016-10-09 17:57:02 +0800

    添加atom/styles.less

--- backup.sh
+++ backup.sh
@@ -1,10 +1,14 @@
-echo '* sublime_text keymap'
-cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap ./sublime_text/
-echo '* sublime_text settings'
-cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings ./sublime_text/
-echo '* zshrc'
-cp ~/.zshrc ./iterm2+zsh/zshrc
-echo '* vimrc'
-cp ~/.vimrc ./vim/vimrc
-echo '* bash_profile'
-cp ~/.bash_profile ./bash_profile
+echo '* atom'
+cp ~/.atom/keymap.cson ./atom/
+cp ~/.atom/config.cson ./atom/
+cp ~/.atom/styles.less ./atom/
+#echo '* sublime_text keymap'
+#cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap ./sublime_text/
+#echo '* sublime_text settings'
+#cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings ./sublime_text/
+#echo '* zshrc'
+#cp ~/.zshrc ./iterm2+zsh/zshrc
+#echo '* vimrc'
+#cp ~/.vimrc ./vim/vimrc
+#echo '* bash_profile'
+#cp ~/.bash_profile ./bash_profile
