464218b11f8b0a4df2afaacb745f1693ebfe36f1
q191201771 <191201771@qq.com>
2017-05-30 11:08:04 +0800

    整理不常关注的内容至dummy目录中

--- backup.sh
+++ backup.sh
@@ -3,13 +3,19 @@ cp ~/.atom/keymap.cson ./atom/
 cp ~/.atom/config.cson ./atom/
 cp ~/.atom/styles.less ./atom/
 ls -lrt ~/.atom/packages | awk 'NF == 9 && $9 != "README.md" {printf "%s-%02s-%s %s\n", $6, $7, $8, $9}' > ./atom/packages_screenshot.md
+
+echo '* vimrc'
+cp ~/.vimrc ./vim/vimrc
+
+echo '* zshrc'
+cp ~/.zshrc ./dummy/iterm2+zsh/zshrc
+
+if [ -d ~/.bash_profile ]; then
+  echo '* bash_profile'
+  cp ~/.bash_profile ./dummy/bash_profile
+fi
+
 #echo '* sublime_text keymap'
 #cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap ./sublime_text/
 #echo '* sublime_text settings'
 #cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings ./sublime_text/
-echo '* zshrc'
-cp ~/.zshrc ./iterm2+zsh/zshrc
-echo '* vimrc'
-cp ~/.vimrc ./vim/vimrc
-#echo '* bash_profile'
-#cp ~/.bash_profile ./bash_profile
